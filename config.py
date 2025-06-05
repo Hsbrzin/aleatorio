4a1086fa0fec92e2633edd0fe347206f1d9e3815
Hsbrzin <albertinaalvez11@gmail.com>
2025-06-04 21:21:34 -0300

    config.py

--- config.py
+++ config.py
@@ -7,18 +7,18 @@
 #6203838763:AAFKHxOaQilei4GnWtvyJ6NPXI03qwhuFI8
 ##```python
 # Your Telegram bot token.
-BOT_TOKEN = "6615803815:AAGGKPkzgIBREPKfsH-QpCMMYVB-8t18gjA"
+BOT_TOKEN = "6300471754:AAFp6xclaS4xjlEfzMzvQ40iljpIg_MViCA"
 
 # Telegram API ID and Hash. This is NOT your bot token and shouldn't be changed.
-API_ID = 9641313
-API_HASH = "baefb797b70643121704c7f344b92870"
+API_ID = 28798075
+API_HASH = "dde9939fb55e753bd8a98446421d024e"
 
 # Chat used for logging errors.
-LOG_CHAT = 5861898630
+LOG_CHAT = -4631480574
 
 # Chat used for logging user actions (like buy, gift, etc).
-ADMIN_CHAT = 6764524229
-GRUPO_PUB = -1001557239664
+ADMIN_CHAT = 6543538187
+GRUPO_PUB = -4631480574
 
 
 # How many updates can be handled in parallel.
@@ -26,23 +26,23 @@ GRUPO_PUB = -1001557239664
 WORKERS = 20
 
 # Admins can access panel and add new materials to the bot.
-ADMINS = [6764524228]
+ADMINS = [6543538187]
 
 # Sudoers have full access to the server and can execute commands.
-SUDOERS = [6764524228]
+SUDOERS = [6543538187]
 
 # All sudoers should be admins too
 ADMINS.extend(SUDOERS)
 
-GIFTERS = [6764524228]
+GIFTERS = [6543538187]
 
 # Bote o Username do bot sem o @
 # Exemplo: default
-BOT_LINK = "Escobar7ShopBot"
+BOT_LINK = "Lojateste2_bot"
 
 
 
 # Bote o Username do suporte sem o @
 # Exemplo: suporte
-BOT_LINK_SUPORTE = "EscobarLaras"
+BOT_LINK_SUPORTE = "Lojateste2_bot"
 ##```
